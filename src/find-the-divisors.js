56b93f115f024b8b7ba1924c42f70d9a5ef4593f
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-21 22:43:02 +0200

    Initial refactor

---
+++ src/find-the-divisors.js
@@ -0,0 +1,5 @@
+function findTheDivisors() {
+  return 'hello';
+}
+
+module.exports = findTheDivisors;
---
+++ src/find-the-divisors.test.js
@@ -0,0 +1,7 @@
+const findTheDivisors = require('./find-the-divisors');
+
+describe('hello', () => {
+  it('should return hello', () => {
+    expect(findTheDivisors()).toBe('hello');
+  });
+});
