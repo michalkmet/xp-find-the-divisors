c2cdbd2d01565be6b8d5b6d61b406db11ee1fe16
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-22 08:05:42 +0200

    UAT1.4 fail

--- src/find-the-divisors.test.js
+++ src/find-the-divisors.test.js
@@ -10,4 +10,7 @@ describe('Story 1', () => {
   it('UAT1.3 - When user pass 6, it should return [2,3]', () => {
     expect(divisors(6)).toStrictEqual([2, 3]);
   });
+  it('UAT1.4 - When user pass 8, it should return [2,4]', () => {
+    expect(divisors(8)).toStrictEqual([2, 4]);
+  });
 });
